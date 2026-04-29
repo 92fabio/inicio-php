2966f35378b02c30b762ffa8bb204b37b6fc1139
92fabio <fabio.bala46@gmail.com>
2026-04-28 22:27:56 -0300

    ARRAY MAP

--- 11_manipulacao_arrays.php
+++ 11_manipulacao_arrays.php
@@ -7,7 +7,7 @@ $nomes[0] = strtoupper($nomes[0]);
 $nomes[1] = strtoupper($nomes[1]);
 $nomes[2] = strtoupper($nomes[2]);
 */
-$nomes = array_map('trasnformaParaMaiuscula', $nomes);
+$nomes = array_map('trasnformaParaMaiuscula', $nomes); // FAZ UM MAPEAMENTO E A CADA ELEMENTO ELE FAZ UMA FUNÇÃO
 
 function trasnformaParaMaiuscula($valor)
 {
