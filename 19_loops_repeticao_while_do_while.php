8302b4a74a2c32e5abcee890563b086e3dd79386
92fabio <fabio.bala46@gmail.com>
2026-05-05 19:35:58 -0300

    ultima aula de laço de repetição while

--- 19_loops_repeticao_while_do_while.php
+++ 19_loops_repeticao_while_do_while.php
@@ -1,8 +1,15 @@
-<?php
+<?php                       // enquanto for verdadeiro faça
 $a = 11;
 
 while ($a <=10) {
     echo $a . "<br>";
     $a++;
 }
+echo '<hr>';
+
+$a = 11;                  //faça enquanto for verdadeiro executa com o 'do'
+do {
+    echo $a;
+    $a++;
+} while ($a <= 10);
 ?>
