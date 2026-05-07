199b40ceb3e11403e67adfde40d365ea066004a9
92fabio <fabio.bala46@gmail.com>
2026-05-06 21:26:58 -0300

    atividade falta concluir

---
+++ exercicios.php
@@ -0,0 +1,24 @@
+<?php
+echo "Boa noite, 2026 é o ano do Rio Brenaco de Americana Abalar as estruturas";
+
+$nome = 'maria';
+echo "Olá, $nome";
+
+$soma = ($100, $300, $500){
+    return $100 + $300 + $500;
+};
+echo soma(100, 300, 500);
+
+resultado = (10 + 5) * 2 / 3 -8;
+echo "O resultado é: $resultado";
+
+$idade = 20;
+if ($idade >= 18){
+    echo "Você é maior de idade";
+}else{
+    echo "Você é menor de idade";
+}
+
+
+
+?>
